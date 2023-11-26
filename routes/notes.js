ccd828432044fd42e3256346ca8535565d997d8b
osonoi <yosonoi@gmail.com>
2023-11-26 19:30:40 +0900

    add cors

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+const cors = require('cors'); // corsミドルウェアを追加
 
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "**********";
+const uri = "mongodb+srv://matsunami2:banana1208@test.zlsie9f.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
+
+// corsミドルウェアを使用
+router.use(cors());
 
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
