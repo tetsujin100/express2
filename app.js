4241c4ced70206eec276cd7d5a03c93fad130a73
osonoi <yosonoi@gmail.com>
2023-12-03 22:10:39 +0900

    add notes_from_b page

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.set('view engine', 'jade');
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 app.use('/cat', catRouter);
+app.use('/notes_from_b', notes_from_bRouter);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
