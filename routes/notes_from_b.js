af82948f85bbadc3c37b0be56255cc8fef72b1d1
osonoi <osonoi@hotmail.com>
2023-12-24 17:08:13 +0900

    change password

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -5,7 +5,7 @@ const cors = require('cors'); // corsミドルウェアを追加
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://password:banana1208@test.zlsie9f.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://matsunami2:password@test.zlsie9f.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
