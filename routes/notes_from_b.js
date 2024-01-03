5b3c0622410856e140d421512a71675c9e6fe298
osonoi <yosonoi@gmail.com>
2024-01-03 23:04:46 +0900

    add env

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
-
+require('dotenv').config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://matsunami2:password@test.zlsie9f.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
